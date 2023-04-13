31581619597df9e459c81d627f64e74ae6195615
Vitaly <vetalxxx2011@mail.ru>
2023-04-13 15:59:16 +0400

    reference solution

--- hw2/task12.py
+++ hw2/task12.py
@@ -16,3 +16,10 @@ if d >= 0:
         print(f'таких чисел не найти')
 else:
     print(f'таких чисел не найти')
+
+# x = int(input())
+# y = int(input())
+# for i in range(x):
+#   for j in range(y):
+#     if x == i + j and y == i * j:
+#       print(i, j)
